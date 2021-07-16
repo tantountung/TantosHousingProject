87dc3d4ce4d6dbcc6cba757485e14d9b3bb2ef96
tanto untung <untung2k3@yahoo.com>
2021-07-16 10:58:35 +0200

    Seeding done, React left

---
+++ TantosHousingProject/Database/DbInitializer.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TantosHousingProject.Database
+{
+    public class DbInitializer
+    {
+        public static void Initialize(
+            THPDbContext context,
+            RoleManager<IdentityRole> roleManager,
+            UserManager<IdentityUser> userManager)
+        {
+            //context.Database.EnsureCreated();
+            //If not using EF migrations
+            context.Database.Migrate();
+
+            if (context.Roles.Any())
+            {
+                return;
+            }
+
+            //------------ Seed into database -------------------
+
+            string[] rolesToSeed = new string[] { "Admin", "User" };
+
+            foreach (var roleName in rolesToSeed)
+            {
+                IdentityRole role = new IdentityRole(roleName);
+
+                var result = roleManager.CreateAsync(role).Result;
+
+                if (!result.Succeeded)
+                {
+                    throw new Exception("Failed to create Role" + roleName);
+                }
+            }
+
+            IdentityUser user = new IdentityUser()
+            {
+                UserName = "AdminPower",
+                Email = "a@a.a",
+                PhoneNumber = "123123"
+            };
+
+            IdentityResult resultUser = userManager.CreateAsync(user, "Qwert!2345").Result;
+
+            if (!resultUser.Succeeded)
+            {
+                throw new Exception("Failed to create Admin Acc: AdminPower");
+
+            }
+
+            IdentityResult resultAssign = userManager.AddToRoleAsync(user, rolesToSeed[0]).Result;
+
+            if (!resultAssign.Succeeded)
+            {
+                throw new Exception($"Failed to grant {rolesToSeed[0]} role to AdminPower");
+
+            }
+        }
+    }
+}
--- TantosHousingProject/Program.cs
+++ TantosHousingProject/Program.cs
@@ -1,11 +1,14 @@
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using TantosHousingProject.Database;
 
 namespace TantosHousingProject
 {
@@ -13,7 +16,9 @@ namespace TantosHousingProject
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            CreateDbIfNotExists(host);
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
@@ -22,5 +27,28 @@ namespace TantosHousingProject
                 {
                     webBuilder.UseStartup<Startup>();
                 });
+
+        private static void CreateDbIfNotExists(IHost host)
+        {
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var context = services.GetRequiredService<THPDbContext>();
+
+                    RoleManager<IdentityRole> roleManager = services.GetRequiredService<RoleManager<IdentityRole>>();
+                    UserManager<IdentityUser> userManager = services.GetRequiredService<UserManager<IdentityUser>>();
+                    
+                    DbInitializer.Initialize(context, roleManager, userManager);
+
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occurred while creating the DB.");
+                }
+            }
+        }
     }
 }
